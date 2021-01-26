7c5c5c5e10e54488a6418ee64579f327bee8be5f
Tigran Galstyan <tigrangalstyan1996@gmail.com>
2021-01-26 17:20:09 +0400

    Refactoring with one loop.

--- myQueue.js
+++ myQueue.js
@@ -30,36 +30,18 @@ function createQ() {
         stack1: createStack(),
         stack2: createStack(),
         enqueue: function (element) {
-
-                this.stack1.push(element);
-
+            this.stack1.push(element);
         },
         dequeue: function () {
-            while (!this.stack1.isEmpty()) {
-                this.stack2.push(this.stack1.pop());
+            if (this.stack2.isEmpty()) {
+                if (this.stack1.isEmpty()) {
+                    return undefined;
+                }
+                while (!this.stack1.isEmpty()) {
+                    this.stack2.push(this.stack1.pop());
+                }
             }
             console.log(this.stack2.pop());
-            while (!this.stack2.isEmpty()) {
-                this.stack1.push(this.stack2.pop());
-            }
-        },
-        printQ: function () {
-            console.log(this.stack1.getData().reverse());
         }
     }
 }
-
-let q = createQ();
-q.enqueue(1);
-q.enqueue(2);
-q.enqueue(3);
-q.enqueue(4);
-q.printQ();
-q.dequeue();
-q.printQ();
-q.dequeue();
-q.printQ();
-q.dequeue();
-q.printQ();
-q.dequeue();
-q.printQ();
